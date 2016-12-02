096af51ec5e0679fd43aabb69157ee61461b2454
Algram <aliasgram@gmail.com>
2016-12-02 19:54:46 +0100

    Day2 Part2 solution

--- day2_part2.js
+++ day2_part2.js
@@ -46,26 +46,40 @@ let posY = 2;
 const position = {
   moveX(num) {
     posX += num;
-    if (posX < 0) posX = 0;
-    if (posX > 2) posX = 2;
+    if (posX === 1 && posY === 0) posX = 2;
+    if (posX === 3 && posY === 0) posX = 2;
+
+    if (posX === 0 && posY === 1) posX = 1;
+    if (posX === 4 && posY === 1) posX = 3;
+
+    if (posX === -1 && posY === 2) posX = 0;
+    if (posX === 5 && posY === 2) posX = 4;
+
+    if (posX === 0 && posY === 3) posX = 1;
+    if (posX === 4 && posY === 3) posX = 3;
+
+    if (posX === 1 && posY === 4) posX = 2;
+    if (posX === 3 && posY === 4) posX = 2;
+
   },
   moveY(num) {
     posY += num;
-    if (posY < 0) posY = 0;
-    if (posY > 2) posY = 2;
+    if (posY === 1 && posX === 0) posY = 2;
+    if (posY === 3 && posX === 0) posY = 2;
+
+    if (posY === 0 && posX === 1) posY = 1;
+    if (posY === 4 && posX === 1) posY = 3;
+
+    if (posY === -1 && posX === 2) posY = 0;
+    if (posY === 5 && posX === 2) posY = 4;
+
+    if (posY === 0 && posX === 3) posY = 1;
+    if (posY === 4 && posX === 3) posY = 3;
+
+    if (posY === 1 && posX === 4) posY = 2;
+    if (posY === 3 && posX === 4) posY = 2;
   },
   getPosition() {
     return `${posX} ${posY}`;
   }
 }
-
-/*
-if (posX === 2 && posY > 0) posY = 2;
-if (posX === 1 && posY > 1) posY = 1;
-if (posX === 0 && posY > 2) posY = 0;
-if (posX === 1 && posY > 3) posY = 1;
-if (posX === 2 && posY > 4) posY = 2;
-if (posX === 3 && posY > 3) posY = 3;
-if (posX === 4 && posY > 2) posY = 4;
-if (posX === 3 && posY > 1) posY = 3;
-*/
