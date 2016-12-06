c05b2bebece8743dcb2bc13eb24549e012063077
Algram <aliasgram@gmail.com>
2016-12-06 18:20:46 +0100

    Solution Day6 Part2

---
+++ day6_part2.js
@@ -0,0 +1,45 @@
+fs = require('fs')
+fs.readFile('day6.input', 'utf8', function (err,data) {
+  if (err) {
+    return console.log(err);
+  }
+  main(data);
+});
+
+function main(data) {
+  const messages = data.split('\n');
+  messages.pop();
+
+  let charArray = [];
+
+  for (let i = 0; i < 8; i++) {
+    for (let message of messages) {
+      const currentChar = message[i];
+
+      if (charArray[i] === undefined) {
+        charArray[i] = '';
+      }
+
+      charArray[i] += currentChar;
+    }
+  }
+
+  for (let char of charArray) {
+    let expCounts = {};
+    let maxKey = '';
+    for(let i = 0; i < char.length; i++) {
+      const key = char[i];
+      if(!expCounts[key]){
+        expCounts[key] = 0;
+      }
+
+      expCounts[key]++;
+
+      if(maxKey == '' || expCounts[key] > expCounts[maxKey]){
+        maxKey = key;
+      }
+    }
+    console.log(expCounts);
+    console.log(maxKey);
+  }
+}
